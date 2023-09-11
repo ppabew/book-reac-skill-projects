6eb9dbbd8f206db1a3d6a14ab83a28ba5712b23e
91274750 <augustman8808@gmail.com>
2023-09-11 09:40:13 +0900

    ⚬ added: Added example codes.

--- chapter6/src/IterationSample.js
+++ chapter6/src/IterationSample.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const IterationSample = () => {
+  const [names, setNames] = useState([
+    { id: 1, text: '눈사람' },
+    { id: 2, text: '얼음' },
+    { id: 3, text: '눈' },
+    { id: 4, text: '바람' },
+  ]);
+
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
+
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    // concat 함수를 사용하여 배열에 추가
+    const nextNames = names.concat({
+      // concat 함수는 새로운 배열을 만들어서 반환합니다.
+      id: nextId, // nextId 값을 id로 설정하고
+      text: inputText,
+    });
+    setNextId(nextId + 1); // nextId 값에 1을 더해 준다.
+    setNames(nextNames); // names 값을 업데이트한다.
+    setInputText(''); // inputText를 비운다.
+  };
+
+  const nameList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
+  const onRemove = (id) => {
+    // filter 함수를 사용하여 배열에 특정 항목을 제거합니다.
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+
   return (
-    <ul>
-      <li>눈사람</li>
-      <li>얼음</li>
-      <li>눈</li>
-      <li>바람</li>
-    </ul>
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>
+    </>
   );
 };
 
---
+++ chapter8/src/App.js
@@ -0,0 +1,22 @@
+import Info from './Info';
+import React, { useState } from 'react';
+
+function App() {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div className="App">
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <br />
+      {visible && <Info />}
+    </div>
+  );
+}
+
+export default App;
